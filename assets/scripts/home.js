18c262a0a0b4eb1c8a23d661b24f256de0fc0907
vkaszuba <codeword.vika@gmail.com>
2020-07-09 18:14:32 -0700

    JS added to redirect home page buttons

---
+++ assets/scripts/home.js
@@ -0,0 +1,10 @@
+
+// Redirects main Design button to its portfolio page
+document.getElementById("designCard").onclick = function () {
+  location.href = "./design.html";
+};
+
+// Redirects main Coding button to its portfolio page
+document.getElementById("codeCard").onclick = function () {
+  location.href = "./coding.html";
+};
