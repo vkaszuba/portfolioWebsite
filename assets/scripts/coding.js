4f22d0db2c793e460824d3cfd404c814668fe7f9
vkaszuba <codeword.vika@gmail.com>
2020-07-09 18:56:54 -0700

    Setting up animations and color shifts for nav on design and code pages

---
+++ assets/scripts/coding.js
@@ -0,0 +1,14 @@
+// Redirects navbar button to home page
+document.getElementById("homeNav").onclick = function () {
+  location.href = "./home.html";
+};
+
+// Redirects navbar button to about page
+document.getElementById("aboutNav").onclick = function () {
+  location.href = "./about.html";
+};
+
+// Redirects navbar button to resume page
+document.getElementById("resumeNav").onclick = function () {
+  location.href = "./resume.html";
+};
