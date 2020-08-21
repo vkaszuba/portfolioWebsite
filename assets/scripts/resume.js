0b22278e3f90ea03ba02fbda3ae9489fb00256ce
vkaszuba <codeword.vika@gmail.com>
2020-08-21 00:26:42 -0700

    SAVED THE SCRIPTS

---
+++ assets/scripts/resume.js
@@ -0,0 +1,24 @@
+// Redirects navbar button to home page
+document.getElementById("homeNav").onclick = function () {
+  location.href = "./home.html";
+};
+
+// Redirects navbar button to design page
+document.getElementById("designNav").onclick = function () {
+  location.href = "./design.html";
+};
+
+// Redirects navbar button to coding page
+document.getElementById("codingNav").onclick = function () {
+  location.href = "./coding.html";
+};
+
+// Redirects navbar button to about page
+document.getElementById("aboutNav").onclick = function () {
+  location.href = "./about.html";
+};
+
+// Redirects navbar button to contact page
+document.getElementById("contactNav").onclick = function () {
+  location.href = "./contact.html";
+};
